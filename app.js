b1151799bcd463fb2f3d40d5c280d761e068d3a2
Eudes89 <eudesmith@gmail.com>
2022-12-12 17:19:27 -0300

    inclusão de novas paginas e config em app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+app.use('/', express.static(__dirname));
+app.use('/produtos', express.static(__dirname + '/produtos.html'));
+app.use('/parceiros', express.static(__dirname + '/parceiros.html'));
+app.use('/sobrenos', express.static(__dirname + '/sobre-nos.html'));
+app.use('/contato', express.static(__dirname + '/contato.html'));
+
+app.listen(3000);
+
+
+
+
--- js/server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-
-app.use('/', express.static(__dirname, 'index'));
-
-
-app.listen(3000);
-
